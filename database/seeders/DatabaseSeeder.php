477d0c4d5f06f851cfe910d261168e70916bfb20
Ibrahim <ibrachar68@gmail.com>
2023-02-15 15:38:17 +0100

    creazione dati nelle tabelle

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,11 +14,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call([
+            genreSeeder::class,
+            tagSeeder::class,
+            movieSeeder::class
+        ]);
     }
 }
